9fb8db46723b48e9331098fefac5c2c130e9593f
Harsh Banka <harshbanka321@gmail.com>
2023-10-24 19:17:31 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import Loaders from './loaders';
 import config from './config';
+import LoggerInstance from './loaders/logger';
 
 async function startServer() {
   const app = express();
   await Loaders({ expressApp: app });
   app
     .listen(config.port, () => {
-      console.log(`
+      LoggerInstance.info(`
       <===== Server listening on port: ${config.port} =====>
     `);
     })
---
+++ src/shared/middlewares/authenticate.ts
@@ -0,0 +1,67 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../jwt";
+import database from "../../loaders/mongo";
+
+// to authenticate faculty
+export default function authenticateFaculty() {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const bearer_token = req.headers["authorization"];
+    console.log(bearer_token);
+    const token = bearer_token?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+    try {
+      const email = verifyToken(token);
+      const db = await database();
+      const faculty = (await db).collection("faculties").findOne({
+        email,
+      });
+      if (!faculty) {
+        return res.status(401).json({
+          message: "Unauthorized",
+        });
+      }
+      res.locals.faculty = faculty;
+      next();
+    } catch (error) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+  };
+}
+
+// to authentiate student
+export function authenticateStudent(){
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const bearer_token = req.headers["authorization"];
+    console.log(bearer_token);
+    const token = bearer_token?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+    try {
+      const email = verifyToken(token);
+      const db = await database();
+      const students = (await db).collection("students").findOne({
+        email,
+      });
+      if (!students) {
+        return res.status(401).json({
+          message: "Unauthorized",
+        });
+      }
+      res.locals.faculty = students;
+      next();
+    } catch (error) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+  }
+}
